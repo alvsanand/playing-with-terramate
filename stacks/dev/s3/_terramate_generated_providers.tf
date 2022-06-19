021effb09699ecfba2b33ebc9747d3cde754a93c
Alvaro Santos Andres <alvsanand@gmail.com>
2022-06-19 18:52:56 +0200

    Removed warning

--- stacks/dev/s3/_terramate_generated_providers.tf
+++ stacks/dev/s3/_terramate_generated_providers.tf
@@ -4,7 +4,7 @@
 provider "aws" {
   access_key                  = "mock_access_key"
   region                      = "eu-west-1"
-  s3_force_path_style         = true
+  s3_use_path_style           = true
   secret_key                  = "mock_secret_key"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
